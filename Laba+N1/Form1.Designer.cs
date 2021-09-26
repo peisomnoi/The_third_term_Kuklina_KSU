13458f9bfe9a21d14e6be1b09ba2f0b0bf13213b
peisomnoi <72002302+peisomnoi@users.noreply.github.com>
2021-09-26 23:34:09 +0700

    1 Lab

---
+++ Laba+N1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Laba_N1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.CalcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CleanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MinimumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "f(x)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            //this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(58, 101);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 26);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            //this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(58, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(112, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            //this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(70, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(33, 261);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(358, 26);
+            this.textBox5.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Результат";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CalcToolStripMenuItem,
+            this.CleanToolStripMenuItem,
+            this.MinimumToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1319, 33);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // CalcToolStripMenuItem
+            // 
+            this.CalcToolStripMenuItem.Name = "CalcToolStripMenuItem";
+            this.CalcToolStripMenuItem.Size = new System.Drawing.Size(107, 29);
+            this.CalcToolStripMenuItem.Text = "Расчитать";
+            this.CalcToolStripMenuItem.Click += new System.EventHandler(this.CalcToolStripMenuItem_Click);
+            // 
+            // CleanToolStripMenuItem
+            // 
+            this.CleanToolStripMenuItem.Name = "CleanToolStripMenuItem";
+            this.CleanToolStripMenuItem.Size = new System.Drawing.Size(103, 29);
+            this.CleanToolStripMenuItem.Text = "Очистить";
+            this.CleanToolStripMenuItem.Click += new System.EventHandler(this.ClearToolStripMenuItem_Click);
+            // 
+            // MinimumToolStripMenuItem
+            // 
+            this.MinimumToolStripMenuItem.Name = "MinimumToolStripMenuItem";
+            this.MinimumToolStripMenuItem.Size = new System.Drawing.Size(203, 29);
+            this.MinimumToolStripMenuItem.Text = "Локальный минимум";
+            this.MinimumToolStripMenuItem.Click += new System.EventHandler(this.MinimumToolStripMenuItem_Click);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(407, 38);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series4.EmptyPointStyle.BackGradientStyle = System.Windows.Forms.DataVisualization.Charting.GradientStyle.LeftRight;
+            series4.EmptyPointStyle.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series4.Legend = "Legend1";
+            series4.Name = "f(x)";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(899, 640);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1319, 692);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem CalcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CleanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem MinimumToolStripMenuItem;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Laba+N1/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MathNet.Symbolics;
+using Expr = MathNet.Symbolics.SymbolicExpression;
+
+namespace Laba_N1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+        }
+
+        private void CalcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double result ;
+            string CalcStr ;
+            chart1.ChartAreas.Clear();
+            chart1.Series.Clear();
+            chart1.ChartAreas.Add(new ChartArea("Function"));
+            Series mySeriesOfPoint = new Series("Function")
+            {
+                ChartType = SeriesChartType.Line,
+                ChartArea = "Function"
+            };
+            double A = Convert.ToDouble(textBox1.Text);
+            double B = Convert.ToDouble(textBox2.Text);
+            double E = Convert.ToDouble(textBox3.Text);
+            if ((A > B) || (E <= 0))
+                {
+                    textBox5.Text="Ошибка ввода";
+                    //break
+                }
+            string FormulaStr = textBox4.Text.ToLower();
+            for (double x = A; x <= B; x += E)
+                {
+                    CalcStr = FormulaStr.Replace("x", x.ToString());
+                try
+                {
+                    //Обходим деление на 0
+                    result = Expr.Parse(CalcStr.Replace(",", ".")).RealNumberValue;
+                    mySeriesOfPoint.Points.AddXY(x, result);
+                }
+                catch {
+                    chart1.Series.Add(mySeriesOfPoint);
+                    mySeriesOfPoint = new Series("Function"+x.ToString())
+                    {
+                        ChartType = SeriesChartType.Line,
+                        ChartArea = "Function"
+                    };
+                    
+                }
+
+                    
+            }
+            chart1.Series.Add(mySeriesOfPoint);
+        }
+
+
+        private void MinimumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double A = Convert.ToDouble(textBox1.Text);
+            double B = Convert.ToDouble(textBox2.Text);
+            double E = Convert.ToDouble(textBox3.Text);
+            double x = A;
+
+            string FormulaStr = textBox4.Text.ToLower();
+            string CalcStr;
+            string CalcStrStart;
+            string CalcStrFinish;
+            double result;
+            double resultStart;
+            double resultFinish;
+            double startspot = A;
+            double finishspot = B;
+            double length = finishspot - startspot;
+
+            while (length > E)
+            {
+                x = (startspot + finishspot) / 2;
+                
+                CalcStr = FormulaStr.Replace("x", x.ToString()).Replace(",", ".");
+                CalcStrStart = FormulaStr.Replace("x", startspot.ToString()).Replace(",",".");
+                CalcStrFinish = FormulaStr.Replace("x", finishspot.ToString()).Replace(",", ".");
+                try
+                {
+                    //Обходим деление на 0
+                    result = Expr.Parse(CalcStr).RealNumberValue;
+                    resultStart = Expr.Parse(CalcStrStart).RealNumberValue;
+                    resultFinish = Expr.Parse(CalcStrFinish).RealNumberValue;
+
+                    if (result > resultStart)
+                    {
+                        finishspot = x;
+                    }
+                    else
+                    {
+                        startspot = x;
+                    }
+                }
+                catch
+                {
+                    finishspot -= E;
+                }
+                // Вычисляем новую длинну.
+                length = (finishspot - startspot);
+            }
+            textBox5.Text = x.ToString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 44 && ch != 45 && ch != 127 && ch != 46)
+            {
+                textBox5.Text = "Ошибка ввода числа A";
+                e.Handled = true;
+            }
+            else {
+                textBox5.Text = "";
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 44 && ch != 45 && ch != 127 && ch != 46)
+            {
+                textBox5.Text = "Ошибка ввода числа B";
+                e.Handled = true;
+            }
+            else
+            {
+                textBox5.Text = "";
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar;
+            if (!Char.IsDigit(ch) && ch != 8 && ch != 44 && ch != 45 && ch != 127 && ch != 46)
+            {
+                textBox5.Text = "Ошибка ввода числа E";
+                e.Handled = true;
+            }
+            else
+            {
+                textBox5.Text = "";
+            }
+        }
+    }
+}
